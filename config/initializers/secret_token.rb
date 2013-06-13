77bc071306d53b5cb1bc253a95e7982d3575d0ea
nerfur <nerfur@crystal.my.domain>
2013-06-13 04:38:17 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supertest::Application.config.secret_token = '5fa803a0fdaf42db785084a5da2787e41fec85434980ed79189af8b612b168455eaa4f977360cccc26cea1179439f9a9983e30aa584665865aeb0a45a83c34c3'
